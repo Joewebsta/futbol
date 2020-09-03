f36826c7ed4722dafbb7066605c9aab2ae75e6f1
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-03 19:26:01 -0400

    Create GameTeamsCollection class.

---
+++ lib/game_teams_collection.rb
@@ -0,0 +1,20 @@
+require './lib/game_teams'
+
+class GameTeamsCollection
+  attr_reader :games_teams, :collection
+
+  def initialize
+    @collection = []
+  end
+
+  def self.from_csv(csv_location)
+    new_games_collection = new
+    new_games_collection.convert_csv(csv_location)
+    new_games_collection.collection
+  end
+
+  def convert_csv(csv_location)
+    games = CSV.read(csv_location, headers: true, header_converters: :symbol)
+    games.each { |row| collection << GameTeams.new(row) }
+  end
+end
