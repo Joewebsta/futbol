c1e0f8b49ae8fefe57d3db9a3e3757eeb9dab2d7
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-03 15:08:09 -0400

    Move Game Statistics methods to GameStatistics module.

---
+++ lib/game_statistics.rb
@@ -0,0 +1,58 @@
+module GameStatistics
+  def highest_total_score
+    games.map { |game| game.away_goals + game.home_goals }.max
+  end
+
+  def lowest_total_score
+    games.map { |game| game.away_goals + game.home_goals }.min
+  end
+
+  def percentage_home_wins
+    tot_games = games.count
+    home_wins = games.select { |game| game.home_goals > game.away_goals }.count.to_f
+    (home_wins / tot_games).round(2)
+  end
+
+  def percentage_visitor_wins
+    tot_games = games.count
+    visitor_wins = games.select { |game| game.away_goals > game.home_goals }.count.to_f
+    (visitor_wins / tot_games).round(2)
+  end
+
+  def percentage_ties
+    (1.0 - percentage_home_wins - percentage_visitor_wins).round(2)
+  end
+
+  def count_of_games_by_season
+    games.sort_by(&:season).each_with_object({}) do |game, hash|
+      hash.default = 0
+      hash[game.season] += 1
+    end
+  end
+
+  def count_of_goals_by_season
+    games.sort_by(&:season).each_with_object({}) do |game, hash|
+      hash.default = 0
+      hash[game.season] += (game.away_goals + game.home_goals)
+    end
+  end
+
+  def average_goals_per_game
+    tot_games = games.count
+    tot_goals = games.reduce(0) do |goal_count, game|
+      goal_count + game.away_goals.to_f + game.home_goals.to_f
+    end
+
+    (tot_goals / tot_games).round(2)
+  end
+
+  def average_goals_by_season
+    tot_goals = count_of_goals_by_season
+    tot_games = count_of_games_by_season
+
+    tot_goals.each_with_object({}) do |season_goals_arr, hash|
+      season = season_goals_arr[0]
+      hash[season] = (tot_goals[season].to_f / tot_games[season]).round(2)
+    end
+  end
+end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,10 @@
 require 'csv'
 require './lib/games_collection'
+require './lib/game_statistics'
 
 class StatTracker
+  include GameStatistics
+
   attr_reader :games, :team_data, :game_teams_data
 
   def initialize(data)
@@ -16,63 +19,6 @@ class StatTracker
     data[:teams] = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
     data[:game_teams] = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
     StatTracker.new(data)
-  end
-
-  def highest_total_score
-    games.map { |game| game.away_goals + game.home_goals }.max
-  end
-
-  def lowest_total_score
-    games.map { |game| game.away_goals + game.home_goals }.min
-  end
-
-  def percentage_home_wins
-    tot_games = games.count
-    home_wins = games.select { |game| game.home_goals > game.away_goals }.count.to_f
-    (home_wins / tot_games).round(2)
-  end
-
-  def percentage_visitor_wins
-    tot_games = games.count
-    visitor_wins = games.select { |game| game.away_goals > game.home_goals }.count.to_f
-    (visitor_wins / tot_games).round(2)
-  end
-
-  def percentage_ties
-    (1.0 - percentage_home_wins - percentage_visitor_wins).round(2)
-  end
-
-  def count_of_games_by_season
-    games.sort_by(&:season).each_with_object({}) do |game, hash|
-      hash.default = 0
-      hash[game.season] += 1
-    end
-  end
-
-  def count_of_goals_by_season
-    games.sort_by(&:season).each_with_object({}) do |game, hash|
-      hash.default = 0
-      hash[game.season] += (game.away_goals + game.home_goals)
-    end
-  end
-
-  def average_goals_per_game
-    tot_games = games.count
-    tot_goals = games.reduce(0) do |goal_count, game|
-      goal_count + game.away_goals.to_f + game.home_goals.to_f
-    end
-
-    (tot_goals / tot_games).round(2)
-  end
-
-  def average_goals_by_season
-    tot_goals = count_of_goals_by_season
-    tot_games = count_of_games_by_season
-
-    tot_goals.each_with_object({}) do |season_goals_arr, hash|
-      season = season_goals_arr[0]
-      hash[season] = (tot_goals[season].to_f / tot_games[season]).round(2)
-    end
   end
 
   # LEAGUE STATS ----------------------------------------------------------------
