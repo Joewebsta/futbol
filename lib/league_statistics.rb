39d3fa93ff16deb2256fe4bccd8216d4bddbb5ac
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-04 14:53:42 -0400

    Reorg/refactor LeaugeStatistics and SeasonStatistics.

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -3,6 +3,42 @@ module LeagueStatistics
     teams.count
   end
 
+  def best_offense
+    best_offense_id = avg_goals_per_game_by_team(game_teams).max_by { |_id, goals| goals }[0]
+    team_name_by_id[best_offense_id]
+  end
+
+  def worst_offense
+    worst_offense_id = avg_goals_per_game_by_team(game_teams).min_by { |_id, goals| goals }[0]
+    team_name_by_id[worst_offense_id]
+  end
+
+  def highest_scoring_visitor
+    away_games_arr = filter_by_hoa('away')
+    highest_scoring_visitor_by_id = avg_goals_per_game_by_team(away_games_arr).max_by { |_id, goals| goals }[0]
+    team_name_by_id[highest_scoring_visitor_by_id]
+  end
+
+  def highest_scoring_home_team
+    home_games = filter_by_hoa('home')
+    high_scoring_home_team_id = avg_goals_per_game_by_team(home_games).max_by { |_id, goals| goals }[0]
+    team_name_by_id[high_scoring_home_team_id]
+  end
+
+  def lowest_scoring_visitor
+    away_games_arr = filter_by_hoa('away')
+    lowest_scoring_visitor_by_id = avg_goals_per_game_by_team(away_games_arr).min_by { |_id, goals| goals }[0]
+    team_name_by_id[lowest_scoring_visitor_by_id]
+  end
+
+  def lowest_scoring_home_team
+    home_games = filter_by_hoa('home')
+    low_scoring_home_team_id = avg_goals_per_game_by_team(home_games).min_by { |_id, goals| goals }[0]
+    team_name_by_id[low_scoring_home_team_id]
+  end
+
+  private
+
   def tot_goals_by_team(data)
     data.sort_by { |game| game.team_id.to_i }.each_with_object({}) do |game, hash|
       hash.default = 0
@@ -33,43 +69,7 @@ module LeagueStatistics
     end
   end
 
-  def best_offense
-    best_offense_arr = avg_goals_per_game_by_team(game_teams).max_by { |_id, goals| goals }
-    best_offense_id = best_offense_arr[0]
-    team_name_by_id[best_offense_id]
-  end
-
-  def worst_offense
-    worst_offense_arr = avg_goals_per_game_by_team(game_teams).min_by { |_id, goals| goals }
-    worst_offense_id = worst_offense_arr[0]
-    team_name_by_id[worst_offense_id]
-  end
-
   def filter_by_hoa(type)
     game_teams.select { |game| game.hoa == type }
-  end
-
-  def highest_scoring_visitor
-    away_games_arr = filter_by_hoa('away')
-    highest_scoring_visitor_by_id = avg_goals_per_game_by_team(away_games_arr).max_by { |_id, goals| goals }[0]
-    team_name_by_id[highest_scoring_visitor_by_id]
-  end
-
-  def lowest_scoring_visitor
-    away_games_arr = filter_by_hoa('away')
-    lowest_scoring_visitor_by_id = avg_goals_per_game_by_team(away_games_arr).min_by { |_id, goals| goals }[0]
-    team_name_by_id[lowest_scoring_visitor_by_id]
-  end
-
-  def highest_scoring_home_team
-    home_games = filter_by_hoa('home')
-    high_scoring_home_team_id = avg_goals_per_game_by_team(home_games).max_by { |_id, goals| goals }[0]
-    team_name_by_id[high_scoring_home_team_id]
-  end
-
-  def lowest_scoring_home_team
-    home_games = filter_by_hoa('home')
-    low_scoring_home_team_id = avg_goals_per_game_by_team(home_games).min_by { |_id, goals| goals }[0]
-    team_name_by_id[low_scoring_home_team_id]
   end
 end
--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -1,4 +1,34 @@
 module SeasonStatistics
+  def winningest_coach(season)
+    win_percent_by_coach_season(season).max_by { |_name, win_percent| win_percent }[0]
+  end
+
+  def worst_coach(season)
+    win_percent_by_coach_season(season).min_by { |_name, win_percent| win_percent }[0]
+  end
+
+  def most_accurate_team(season)
+    most_accurate_team = accuracy_by_team_season(season).max_by { |_id, accuracy| accuracy }[0]
+    team_name_by_id[most_accurate_team]
+  end
+
+  def least_accurate_team(season)
+    least_accurate_team = accuracy_by_team_season(season).min_by { |_id, accuracy| accuracy }[0]
+    team_name_by_id[least_accurate_team]
+  end
+
+  def most_tackles(season)
+    most_tackles_team_id = tackles_by_team_season(season).max_by { |_id, accuracy| accuracy }[0]
+    team_name_by_id[most_tackles_team_id]
+  end
+
+  def fewest_tackles(season)
+    fewest_tackles_team_id = tackles_by_team_season(season).min_by { |_id, accuracy| accuracy }[0]
+    team_name_by_id[fewest_tackles_team_id]
+  end
+
+  private
+
   def team_ids
     game_teams.map(&:team_id).sort_by(&:to_i).uniq
   end
@@ -32,14 +62,6 @@ module SeasonStatistics
     end
   end
 
-  def winningest_coach(season)
-    win_percent_by_coach_season(season).max_by { |_name, win_percent| win_percent }[0]
-  end
-
-  def worst_coach(season)
-    win_percent_by_coach_season(season).min_by { |_name, win_percent| win_percent }[0]
-  end
-
   def matches_season?(game, season)
     game.game_id[0..3] == season[0..3]
   end
@@ -68,30 +90,10 @@ module SeasonStatistics
     end
   end
 
-  def most_accurate_team(season)
-    most_accurate_team = accuracy_by_team_season(season).max_by { |id_accuracy_arr| id_accuracy_arr[1] }[0]
-    team_name_by_id[most_accurate_team]
-  end
-
-  def least_accurate_team(season)
-    least_accurate_team = accuracy_by_team_season(season).min_by { |id_accuracy_arr| id_accuracy_arr[1] }[0]
-    team_name_by_id[least_accurate_team]
-  end
-
   def tackles_by_team_season(season)
     game_teams.each_with_object({}) do |game, hash|
       hash.default = 0
       hash[game.team_id] += game.tackles.to_i if matches_season?(game, season)
     end
-  end
-
-  def most_tackles(season)
-    most_tackles_team_id = tackles_by_team_season(season).max_by { |id_tackles_arr| id_tackles_arr[1] }[0]
-    team_name_by_id[most_tackles_team_id]
-  end
-
-  def fewest_tackles(season)
-    fewest_tackles_team_id = tackles_by_team_season(season).min_by { |id_tackles_arr| id_tackles_arr[1] }[0]
-    team_name_by_id[fewest_tackles_team_id]
   end
 end
